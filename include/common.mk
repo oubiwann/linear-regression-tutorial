e62304e31c40e438bb14770e5211351d1239a867
Duncan M. McGreggor <oubiwann@cogitat.io>
2014-11-28 20:52:23 -0600

    Changes for using the new Hy kernel.

--- include/common.mk
+++ include/common.mk
@@ -4,7 +4,7 @@ HY=hy
 IPYTHON=ipython3
 IPYTHON_DIR=$(shell pwd)/.ipython
 IPYTHON_PROFILE=ihy
-IPYTHON_OPTIONS=--ipython-dir=$(IPYTHON_DIR) --profile=$(IPYTHON_PROFILE)
+IPYTHON_OPTIONS=--ipython-dir=$(IPYTHON_DIR) --profile=$(IPYTHON_PROFILE) --kernel hy
 SYSTEM_PYTHON=$(shell which $(PYTHON))
 SOURCE=./lib
 
